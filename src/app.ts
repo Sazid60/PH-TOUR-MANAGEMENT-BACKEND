934bfce7c77e2dbbb74eed2cae633d0ed22ffc54
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-07-09 21:10:11 +0600

    Add:  26-6 Setting Up Global Error Handler

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import express, { Request, Response } from "express"
 import cors from "cors"
 
 import { router } from "./app/routes"
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler"
 
 const app = express()
 app.use(express.json())
@@ -11,6 +12,8 @@ app.use(cors())
 
 app.use("/api/v1", router)
 
+// using the global error handler 
+app.use(globalErrorHandler)
 
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,17 +1,17 @@
-/* eslint-disable no-console */
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 import httpStatus from "http-status-codes"
 
 import { userServices } from "./user.service";
 
-const createUser = async (req: Request, res: Response) => {
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
     try {
 
         // using the service 
-        const user = userServices.createUser(req.body)
+        const user = await userServices.createUser(req.body)
 
         res.status(httpStatus.CREATED).json({
             message: "User Created Successfully",
@@ -19,11 +19,14 @@ const createUser = async (req: Request, res: Response) => {
         })
 
     } catch (err: any) {
-        console.log(err)
-        res.status(httpStatus.BAD_REQUEST).json({
-            message: `Something went wrong ${err?.message}`,
-            err
-        })
+        // console.log(err)
+        // res.status(httpStatus.BAD_REQUEST).json({
+        //     message: `Something went wrong ${err?.message}`,
+        //     err
+        // })
+
+        // will take to global error handler 
+        next(err)
     }
 
 }
