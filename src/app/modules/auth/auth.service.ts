740240a75a15c0094dbcea5e01bd62ca7a29150b
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-07-11 14:46:42 +0600

    27-5 Create Login API

---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import AppError from "../../errorHelpers/AppError"
+import { IUser } from "../user/user.interface"
+import httpStatus from 'http-status-codes';
+import { User } from "../user/user.model";
+import bcrypt from "bcryptjs";
+
+
+const credentialsLogin = async (payload: Partial<IUser>) => {
+    const { email, password } = payload
+
+    const isUserExist = await User.findOne({ email })
+    if (!isUserExist) {
+        throw new AppError(httpStatus.BAD_REQUEST, "Email Does Not Exist")
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password as string, isUserExist.password as string)
+
+    if (!isPasswordMatch) {
+        throw new AppError(httpStatus.BAD_REQUEST, "Password Does Not Match")
+    }
+
+    return {
+        email: isUserExist.email
+    }
+}
+
+export const AuthServices = {
+    credentialsLogin
+}
