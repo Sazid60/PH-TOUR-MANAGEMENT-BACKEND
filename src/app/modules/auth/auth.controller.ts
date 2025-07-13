e15681ac86968e7e7223d160e1b28d5520582fc8
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-07-13 23:31:21 +0600

    28-4 Create utility functions for getting accessToken and refreshToken and setting browser cookies

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -6,22 +6,27 @@ import httpStatus from 'http-status-codes';
 import { AuthServices } from "./auth.service";
 import { catchAsync } from "../../utils/catchAsync";
 import AppError from "../../errorHelpers/AppError";
+import { setAuthCookie } from "../../utils/setCookie";
+
 
 const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const loginInfo = await AuthServices.credentialsLogin(req.body)
 
-    res.cookie("accessToken", loginInfo.accessToken,
-        {
-            httpOnly: true,
-            secure: false
-        }
-    )
-    res.cookie("refreshToken", loginInfo.refreshToken,
-        {
-            httpOnly: true, // this is for setting the cookies in frontend 
-            secure: false //because for security issue frontend normally do not allow to set cookies because backend and frontend have two different live server 
-        }
-    )
+    // res.cookie("accessToken", loginInfo.accessToken,
+    //     {
+    //         httpOnly: true,
+    //         secure: false
+    //     }
+    // )
+    // res.cookie("refreshToken", loginInfo.refreshToken,
+    //     {
+    //         httpOnly: true, // this is for setting the cookies in frontend 
+    //         secure: false //because for security issue frontend normally do not allow to set cookies because backend and frontend have two different live server 
+    //     }
+    // )
+
+    // both access token and refresh token works will be done by this function 
+    setAuthCookie(res, loginInfo)
 
     // (method) Response<any, Record<string, any>, number>.cookie(name: string, val: string, options: CookieOptions): Response<any, Record<string, any>> (+2 overloads)
 
@@ -39,6 +44,9 @@ const getNewAccessToken = catchAsync(async (req: Request, res: Response, next: N
         throw new AppError(httpStatus.BAD_REQUEST, "No Access Token Received")
     }
     const tokenInfo = await AuthServices.getNewAccessToken(refreshToken)
+    // this will set the newly generated access token (generated using refresh token) to the cookies
+
+    setAuthCookie(res, tokenInfo)
 
     sendResponse(res, {
         success: true,
