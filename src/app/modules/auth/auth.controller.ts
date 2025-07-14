7dbdc023c71aff6f71174a7904597f0e56ebc67c
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-07-14 12:15:27 +0600

    28-6 Implement Reset / Change Password

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -7,6 +7,7 @@ import { AuthServices } from "./auth.service";
 import { catchAsync } from "../../utils/catchAsync";
 import AppError from "../../errorHelpers/AppError";
 import { setAuthCookie } from "../../utils/setCookie";
+import bcrypt from 'bcryptjs';
 
 
 const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
@@ -71,6 +72,8 @@ const logout = catchAsync(async (req: Request, res: Response, next: NextFunction
         }
     )
 
+    // (method) Response<any, Record<string, any>, number>.clearCookie(name: string, options?: CookieOptions): Response<any, Record<string, any>>
+
     sendResponse(res, {
         success: true,
         statusCode: httpStatus.OK,
@@ -79,12 +82,27 @@ const logout = catchAsync(async (req: Request, res: Response, next: NextFunction
     })
 
 })
-// (method) Response<any, Record<string, any>, number>.clearCookie(name: string, options?: CookieOptions): Response<any, Record<string, any>>
+const resetPassword = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const newPassword = req.body.newPassword
+    const oldPassword = req.body.oldPassword
+    const decodedToken = req.user
 
+    await AuthServices.resetPassword(oldPassword, newPassword, decodedToken)
+
+
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "password Changed Successfully",
+        data: null
+    })
+
+})
 
 
 export const AuthControllers = {
     credentialsLogin,
     getNewAccessToken,
-    logout
+    logout,
+    resetPassword
 }
