44422f59beb054e2c481258dfce8204c80109c7a
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-07-19 13:27:21 +0600

    30-8 Refactor your code and build a Query Builder

--- src/app/modules/tour/tour.controller.ts
+++ src/app/modules/tour/tour.controller.ts
@@ -16,7 +16,7 @@ const createTour = catchAsync(async (req: Request, res: Response) => {
 
 const getAllTours = catchAsync(async (req: Request, res: Response) => {
     const query = req.query
-    const result = await TourService.getAllTours(query as Record<string, unknown>);
+    const result = await TourService.getAllTours(query as Record<string, string>);
     sendResponse(res, {
         statusCode: 200,
         success: true,
