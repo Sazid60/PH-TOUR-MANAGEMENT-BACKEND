d8c0bc0afa6e0e1ea55a94fa63175cfd7865e60e
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-07-20 17:00:08 +0600

    31-4 Use Transaction and Rollback for Booking and Payment

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -16,46 +16,79 @@ const createBooking = async (payload: Partial<IBooking>, userId: string) => {
 
     const transactionId = getTransactionId()
 
-    const user = await User.findById(userId)
-    if (!user?.phone || !user?.address) {
-        throw new AppError(httpStatus.BAD_REQUEST, "Please Add Phone Number and Address In Your Profile For Booking!")
+    // create a session over Booking model since all are happening over booking module 
+
+    // 1. start session
+    const session = await Booking.startSession()
+
+    //2.  start transaction 
+    session.startTransaction()
+
+    // inside the try do all the operation and business logic 
+    try {
+        // 
+
+        const user = await User.findById(userId)
+        if (!user?.phone || !user?.address) {
+            throw new AppError(httpStatus.BAD_REQUEST, "Please Add Phone Number and Address In Your Profile For Booking!")
+        }
+
+        const tour = await Tour.findById(payload.tour).select("costFrom")
+
+        if (!tour?.costFrom) {
+            throw new AppError(httpStatus.BAD_REQUEST, "Tour Cost is Not Added!, Wait Until Cost Is Added!")
+        }
+
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const amount = Number(tour.costFrom) * Number(payload.guestCount!)
+
+
+
+        const booking = await Booking.create([{
+            user: userId,
+            status: BOOKING_STATUS.PENDING,
+            ...payload
+        }], { session })
+
+
+
+        const payment = await Payment.create(
+            [
+                {
+                    booking: booking[0]._id,
+                    status: PAYMENT_STATUS.UNPAID,
+                    transactionId: transactionId,
+                    amount
+                }
+            ],
+            { session }
+        )
+
+        const updatedBooking = await Booking
+            .findByIdAndUpdate(
+                booking[0]._id,
+                { payment: payment[0]._id },
+                { new: true, runValidators: true, session }
+            )
+            .populate("user", "name email phone address")
+            .populate("tour", "title costFrom")
+            .populate("payment");
+
+
+        // After success commit the transaction and end the transaction 
+        // here committing means inserting all the operation data to actual db from virtual database copy. 
+        await session.commitTransaction(); //transaction
+        session.endSession()
+
+        return updatedBooking
+    } catch (error) {
+        // inside the catch handle the error of the session and aborting the session 
+        await session.abortTransaction()
+        // throw new AppError(httpStatus.BAD_REQUEST, error) ❌❌
+        throw error
+        //  here we do not need to use our custom AppError because mongoose already has the error pattern for this and our AppError Do Not know about the error. Mongoose does the works for us. 
     }
 
-    const tour = await Tour.findById(payload.tour).select("costFrom")
-
-    if (!tour?.costFrom) {
-        throw new AppError(httpStatus.BAD_REQUEST, "Tour Cost is Not Added!, Wait Until Cost Is Added!")
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const amount = Number(tour.costFrom) * Number(payload.guestCount!)
-
-
-
-    const booking = await Booking.create({
-        user: userId,
-        status: BOOKING_STATUS.PENDING,
-        ...payload
-    })
-
-    const payment = await Payment.create({
-        booking: booking._id,
-        status: PAYMENT_STATUS.UNPAID,
-        transactionId: transactionId,
-        amount
-
-    })
-
-    const updatedBooking = await Booking.findByIdAndUpdate(
-        booking._id,
-        { payment: payment._id },
-        { runValidators: true, new: true }
-    )
-        .populate("user", "name email address")
-        .populate("tour", "title costFrom")
-        .populate("payment")
-
-    return updatedBooking
 };
 
 const getUserBookings = async () => {
