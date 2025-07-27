5bd817740ab0b3b8493aac5858fa685c28fdbcef
SHAHNAWAZ SAZID <shahnawazsazid60@gmail.com>
2025-07-28 02:31:27 +0600

    Delete dist directory

--- dist/app/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.excludedFields = void 0;
-exports.excludedFields = ["searchTerm", "sort", "fields", "page", "limit"];
